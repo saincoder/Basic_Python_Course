ab78fdb301d015ed5eea0c22b28701c8ca0042c4
shaid03 <kancain00@gmail.com>
2024-01-28 11:37:39 +0500

    right angle triangle

--- patterns_programs_right_angle_T.py
+++ patterns_programs_right_angle_T.py
@@ -10,3 +10,12 @@ for i in range(1, 9):
     for j in range(1, 9 - i + 1):
         print(' * ', end='')
     print()
+
+
+print()
+k = 0
+for i in range(1, 5):
+    for j in range(1, 5 - i + 1):
+        print(k, ' ', end='')
+        k = k+1
+    print()
